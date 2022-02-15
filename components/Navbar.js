16373f029a48061f1722085e683fa45779fe05a6
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-02-15 14:12:45 +0100

    First

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+// import styles from '../styles/Home.module.css'
+
+// export default function Navbar() {
+//     return (
+//         <nav className={styles.navbar}>
+//         {/* <Image src={logo} alt="logo" width = {143} height={40} /> */}
+ 
+//          <select>
+//            <option hidden selected disabled value="0">English</option>
+//            <option>العربية</option>
+//            <option>Francais</option>
+//            <option>English</option>
+//          </select>
+       
+//        </nav>
+//     )
+// }
