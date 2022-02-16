e7032a9818a56d2f8244d121423b20c433c91784
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-02-16 13:20:44 +0100

    Add

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  
   reactStrictMode: true,
+  env :{
+    GITHUB_CLIENT_ID : "874168a3c77b9300bf31",
+    GITHUB_CLIENT_SECRET : "de9b0244c18e7dd25c697e9084946450d8601698"
+  }
 }
+
